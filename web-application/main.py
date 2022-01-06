9e9263ad43861f58e4186e911fb4b8680394e01b
Luffy-Yao <56039326+Luffy-Yao@users.noreply.github.com>
2022-01-05 21:43:19 -0600

    current

--- web-application/main.py
+++ web-application/main.py
@@ -1,188 +1,131 @@
-#!/usr/bin/env python
 from flask import Flask, render_template, flash, request, jsonify, Markup
+from model import Inventory_Simulation
+import collections
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import io, base64, os
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
- 
 
-# default traveler constants
-DEFAULT_EMBARKED = 'Southampton'
-DEFAULT_FARE = 33
-DEFAULT_AGE = 30
-DEFAULT_GENDER = 'Female'
-DEFAULT_TITLE = 'Mrs.'
-DEFAULT_CLASS = 'Second'
-DEFAULT_CABIN = 'C'
-DEFAULT_SIBSP = 0
-DEFAULT_PARCH = 0
 
-# initializing constant vars
-average_survival_rate = 0
-# logistic regression modeling
-lr_model = LogisticRegression()
+# default User Input
+INVENTORY_CHECK_FREQUENCY = 1 # A value of 1 indicates that the stock is checked at the end of each day.
+
+# Main User Inputs
+STARTING_STOCK = 50
+REORDER_THRESHOLD = 30 # Use a value of float("inf") in order to simulate the behavior of periodic checks
+ORDER_FILL_AMOUNT = 100 # This strategy uses a constant value when placing an order.
+LEAD_TIME = 5
+UNIT_COST = 3
+SELLING_PRICE = 10
+HOLDING_COST = 0.01
+ORDER_COST = 1
+
+
+# Defining different types of records used throughout the simulation.
+EndOfTimeStepRecord = collections.namedtuple(
+    "EndOfTimeStepRecord", ("date", "ending_stock")
+)
+OrderPlacementRecord = collections.namedtuple(
+    "OrderPlacementRecord", ("arrival_date", "units_ordered", "order_placement_date")
+)
+SalesRecord = collections.namedtuple("SalesRecord", ("date", "units_sold"))
+LossRecord = collections.namedtuple("LossRecord", ("date", "units_lost"))
+
+# A generic object used for some visualizations
+HorizontalLineInfo = collections.namedtuple("HorizontalLineInfo", ("label", "value"))
 
 app = Flask(__name__)
-
-
-@app.before_first_request
-def startup():
-    global average_survival_rate, lr_model 
-
-    from numpy import genfromtxt
-    titanic_array = genfromtxt('titanic3.csv', delimiter=',')
-    average_survival_rate = (np.mean([item[0] for item in titanic_array]) * 100)
-
-    X_train, X_test, y_train, y_test = train_test_split([item[1:] for item in titanic_array], 
-                                                 [item[0] for item in titanic_array], test_size=0.5, random_state=42)
-
-
-    # fit model only once
-    lr_model.fit(X_train, y_train)
 
 @app.route("/", methods=['POST', 'GET'])
 def submit_new_profile():
     model_results = ''
     if request.method == 'POST':
-        selected_embarked = request.form['selected_embarked']
-        selected_fare = request.form['selected_fare']
-        selected_age = request.form['selected_age']
-        selected_gender = request.form['selected_gender']
-        selected_title = request.form['selected_title']
-        selected_class = request.form['selected_class']
-        selected_cabin = request.form['selected_cabin']
-        selected_sibsp = request.form['selected_sibsp']
-        selected_parch = request.form['selected_parch']
+        selected_startingStock = request.form['starting_stock']
+        selected_reorderThreshold = request.form['reorder_threshold']
+        selected_orderFillAmount = request.form['order_fill_amount']
+        selected_leadTime = request.form['lead_time']
+        selected_unitCost = request.form['unit_cost']
+        selected_sellingPrice = request.form['selling_price']
+        selected_holdingCost = request.form['holding_cost']
+        selected_orderCost = request.form['order_cost']
 
-        # assign new variables to live data for prediction
-        age = int(selected_age)
-        isfemale = 1 if selected_gender == 'Female' else 0
-        sibsp = int(selected_sibsp)
-        parch = int(selected_parch)
-        fare = int(selected_fare)
-        
-        # point of embarcation
-        embarked_Q = 1
-        embarked_S = 0
-        embarked_Unknown = 0 
-        embarked_nan = 0
-        if (selected_embarked[0]=='Q'):
-            embarked_Q = 1
-        if (selected_embarked[0]=='S'):
-            embarked_S = 1
-
-        # class
-        pclass_Second = 0
-        pclass_Third = 0
-        pclass_nan = 0
-        if (selected_class=='Second'):
-            pclass_Second = 0
-        if (selected_class=='Third'):
-            pclass_Third = 0
-
-        # title
-        title_Master = 0
-        title_Miss = 0
-        title_Mr = 0
-        title_Mrs = 0
-        title_Rev = 0
-        title_Unknown = 0
-        title_nan = 0
-        if (selected_title=='Master.'):
-            title_Master = 1
-        if (selected_title=='Miss.'):
-            title_Miss = 1
-        if (selected_title=='Mr.'):
-            title_Mr = 1
-        if (selected_title=='Mrs.'):
-            title_Mrs = 1
-        if (selected_title=='Rev.'):
-            title_Master = 1
-        if (selected_title=='Unknown'):
-            title_Unknown = 1
- 
-        # cabin
-        cabin_B = 0
-        cabin_C = 0  
-        cabin_D = 0  
-        cabin_E = 0
-        cabin_F = 0
-        cabin_G = 0
-        cabin_T = 0
-        cabin_Unknown = 0
-        cabin_nan = 0
-        if (selected_cabin=='B'):
-            cabin_B = 1
-        if (selected_cabin=='C'):
-            cabin_C = 1
-        if (selected_cabin=='D'):
-            cabin_D = 1
-        if (selected_cabin=='E'):
-            cabin_E = 1
-        if (selected_cabin=='F'):
-            cabin_F = 1
-        if (selected_cabin=='G'):
-            cabin_G = 1
-        if (selected_cabin=='T'):
-            cabin_T = 1
-        if (selected_cabin=='Unknown'):
-            cabin_Unknown = 1
  
         # build new array to be in same format as modeled data so we can feed it right into the predictor
-        user_designed_passenger = [[age, sibsp, parch, fare, isfemale, pclass_Second, pclass_Third, pclass_nan, cabin_B, cabin_C, cabin_D, cabin_E, cabin_F, cabin_G, cabin_T, cabin_Unknown, cabin_nan, embarked_Q, embarked_S, embarked_Unknown, embarked_nan, title_Master, title_Miss, title_Mr, title_Mrs, title_Rev, title_Unknown, title_nan]]
+        simulation = Inventory_Simulation()
+        INVENTORY_CHECK_FREQUENCY = 1 # A value of 1 indicates that the stock is checked at the end of each day.
  
-
+        strategy_params = {
+        "lead_time": float(selected_leadTime),
+        "reorder_threshold": float(selected_reorderThreshold),
+        "order_fill_amount_function": lambda : float(selected_orderFillAmount),
+    }
         # add user desinged passenger to predict function
-        Y_pred = lr_model.predict_proba(user_designed_passenger)
-        probability_of_surviving_fictional_character = Y_pred[0][1] * 100
+        historical_data = simulation.generate_historical_data() # NOTE: This is currently a placeholder that will be needed in a real environment; it is not used for the purposes of this simulation.
+        forecasted_data = simulation.generate_forecasted_data()
+
+        
+        simulation_data = simulation.simulate(
+        float(selected_startingStock),
+        forecasted_data,
+        inventory_check_frequency=INVENTORY_CHECK_FREQUENCY,
+        strategy_params=strategy_params,
+)
+
+        
+        FAKE_PRODUCT_INFO = {
+                "unit_cost": int(selected_unitCost),
+                "selling_price": int(selected_sellingPrice),
+                "holding_cost": float(selected_holdingCost),
+                "order_cost": int(selected_orderCost),
+            }
+        simulation_profit = simulation.calculate_profit(simulation_data, FAKE_PRODUCT_INFO)
+
+        simulation_loss = simulation.calculate_losses(simulation_data, FAKE_PRODUCT_INFO)
+        restock_line = HorizontalLineInfo(label="Restock Threshold", value=REORDER_THRESHOLD)
 
         fig = plt.figure()
-        objects = ('Average Survival Rate', 'Fictional Traveler')
-        y_pos = np.arange(len(objects))
-        performance = [average_survival_rate, probability_of_surviving_fictional_character]
 
         ax = fig.add_subplot(111)
-        colors = ['gray', 'blue']
-        plt.bar(y_pos, performance, align='center', color = colors, alpha=0.5)
-        plt.xticks(y_pos, objects)
-        plt.axhline(average_survival_rate, color="r")
-        plt.ylim([0,100])
-        plt.ylabel('Survival Probability')
-        plt.title('How Did Your Fictional Traveler Do? \n ' + str(round(probability_of_surviving_fictional_character,2)) + '% of Surviving!')
+        G = simulation_data["time_step_stock_counts"]
+
+        plt.plot(list(p.ending_stock for p in G), label="Inventory")
+        horizontal_line_info = restock_line
+        if horizontal_line_info is not None:
+            plt.plot([horizontal_line_info.value] * len(G), label=horizontal_line_info.label)
+        plt.legend(loc="upper left")
+        plt.title("Inventory Levels")
+        plt.xlabel("Time Step")
+        plt.ylabel("Inventory")
         img = io.BytesIO()
         plt.savefig(img, format='png')
         img.seek(0)
         plot_url = base64.b64encode(img.getvalue()).decode()
 
         return render_template('index.html',
-            model_results = model_results,
+            simulated_profit = simulation_profit,
+            simulated_loss = simulation_loss,
             model_plot = Markup('<img src="data:image/png;base64,{}">'.format(plot_url)),
-            selected_embarked = selected_embarked,
-            selected_fare = selected_fare,
-            selected_age = selected_age,
-            selected_gender = selected_gender,
-            selected_title = selected_title,
-            selected_class = selected_class,
-            selected_cabin = selected_cabin,
-            selected_sibsp = selected_sibsp,
-            selected_parch = selected_parch)
+            selected_startingStock = request.form['starting_stock'],
+            selected_reorderThreshold = request.form['reorder_threshold'],
+            selected_orderFillAmount = request.form['order_fill_amount'],
+            selected_leadTime = request.form['lead_time'],
+            selected_unitCost = request.form['unit_cost'],
+            selected_sellingPrice = request.form['selling_price'],
+            selected_holdingCost = request.form['holding_cost'],
+            selected_orderCost = request.form['order_cost'])
     else:
         # set default passenger settings
         return render_template('index.html',
-            model_results = '',
             model_plot = '',
-            selected_embarked = DEFAULT_EMBARKED,
-            selected_fare = DEFAULT_FARE,
-            selected_age = DEFAULT_AGE,
-            selected_gender = DEFAULT_GENDER,
-            selected_title = DEFAULT_TITLE,
-            selected_class = DEFAULT_CLASS,
-            selected_cabin = DEFAULT_CABIN,
-            selected_sibsp = DEFAULT_SIBSP,
-            selected_parch = DEFAULT_PARCH)
+            selected_startingStock = STARTING_STOCK ,
+            selected_reorderThreshold = REORDER_THRESHOLD,
+            selected_orderFillAmount = ORDER_FILL_AMOUNT,
+            selected_leadTime = LEAD_TIME,
+            selected_unitCost = UNIT_COST,
+            selected_sellingPrice = SELLING_PRICE,
+            selected_holdingCost = HOLDING_COST,
+            selected_orderCost = ORDER_COST)
 
 if __name__=='__main__':
 	app.run(debug=False)
---
+++ web-application/model.py
@@ -0,0 +1,221 @@
+import collections
+import heapq
+import logging
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+logging.basicConfig(
+    # filename="log.log",
+    # filemode="a",
+    format="%(asctime)s %(levelname)-8s [%(name)s] : %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logger = logging.getLogger(__name__)
+
+
+
+# Defining different types of records used throughout the simulation.
+EndOfTimeStepRecord = collections.namedtuple(
+    "EndOfTimeStepRecord", ("date", "ending_stock")
+)
+OrderPlacementRecord = collections.namedtuple(
+    "OrderPlacementRecord", ("arrival_date", "units_ordered", "order_placement_date")
+)
+SalesRecord = collections.namedtuple("SalesRecord", ("date", "units_sold"))
+LossRecord = collections.namedtuple("LossRecord", ("date", "units_lost"))
+
+# A generic object used for some visualizations
+HorizontalLineInfo = collections.namedtuple("HorizontalLineInfo", ("label", "value"))
+
+
+class Inventory_Simulation(object):
+    # Generate / create an input signal
+    def generate_normal_input(self, mean=10, std=3, size=100, floor_value=0, round_input=True):
+        normal_input = np.random.normal(loc=mean, scale=std, size=size)
+        
+        # Truncate all entries using the given floor_value (e.g., floor_value=0 means negative values are not allowed).
+        if floor_value is not None:
+            normal_input[normal_input <= floor_value] = floor_value
+        
+        # A boolean flag used to round values.  The default rounding strategy will round inputs with decimal values of 0.5 and below down to the nearest integer; inputs with higher decimal values will be rounded up to the next highest integer value.
+        if round_input:
+            normal_input = np.round(normal_input)
+        return normal_input
+
+
+    def generate_historical_data(self, *args, **kwargs):
+        historical_data = self.generate_normal_input(*args, **kwargs)
+        return historical_data
+
+    def generate_forecasted_data(self, *args, **kwargs):
+        forecasted_data = self.generate_normal_input(*args, **kwargs)
+        return forecasted_data
+
+
+    def calculate_profit(self, simulation_data, product_info):
+        unit_cost = product_info["unit_cost"]
+        selling_price = product_info["selling_price"]
+        holding_cost = product_info["holding_cost"]
+        order_cost = product_info["order_cost"]
+
+        revenue = sum(r.units_sold for r in simulation_data["units_sold"]) * selling_price
+        order_costs = len(simulation_data["orders_placed"]) * order_cost
+        holding_costs = (
+            sum(r.ending_stock for r in simulation_data["time_step_stock_counts"])
+            * holding_cost
+        )
+        unit_costs = (
+            sum(r.units_ordered for r in simulation_data["orders_placed"]) * unit_cost
+        )
+
+        profit = revenue - order_costs - holding_costs - unit_costs
+
+        return profit
+
+    def calculate_losses(self, simulation_data, product_info):
+        unit_cost = product_info["unit_cost"]
+        selling_price = product_info["selling_price"]
+
+        units_lost = sum(r.units_lost for r in simulation_data["units_lost"])
+
+        loss = units_lost * (selling_price - unit_cost)
+        return loss
+
+
+    def plot_inventory_graph(self, simulation_data, horizontal_line_info=None):
+        G = simulation_data["time_step_stock_counts"]
+        plt.plot(list(p.ending_stock for p in G), label="Inventory")
+
+        if horizontal_line_info is not None:
+            plt.plot([horizontal_line_info.value] * len(G), label=horizontal_line_info.label)
+        plt.legend(loc="upper left")
+        plt.title("Inventory Levels")
+        plt.xlabel("Time Step")
+        plt.ylabel("Inventory")
+        plt.show()
+
+    def threshold_restock_strategy(self, 
+        current_stock,
+        reorder_threshold,
+        order_fill_amount_function,
+        lead_time,
+        current_time_step,
+        order_fill_params=None,
+    ):
+        """
+        When `current_stock` falls below the `reorder_threshold` create an order for `order_fill_amount` units.
+        """
+        order_placed = None
+
+        if order_fill_params is None:
+            order_fill_amount = order_fill_amount_function()
+        else:
+            order_fill_amount = order_fill_amount_function(current_stock, **order_fill_params)
+        if (current_stock <= reorder_threshold) and (order_fill_amount > 0):
+
+            order_placed = OrderPlacementRecord(
+                arrival_date=current_time_step + lead_time,
+                units_ordered=order_fill_amount,
+                order_placement_date=current_time_step,
+            )
+
+        return order_placed
+
+
+    # Simulate activity using the restocking strategy
+
+    def simulate(self,
+        starting_stock,
+        sales_data,
+        inventory_check_frequency,
+        strategy_params,
+    ):
+        """
+        Simulate the activity based on the given parameters.
+        """
+        simulation_data = {
+            "time_step_stock_counts": [],
+            "orders_placed": [],
+            "units_sold": [],
+            "units_lost": [],
+        }
+        current_stock = starting_stock
+        open_orders_heap = []
+
+        for time_step, sales in enumerate(sales_data):
+
+            # Check if any orders have been delievered and add them to the stock
+            # NOTE: This current model makes the assumption that orders arrive at the start of the time period.
+            if len(open_orders_heap) > 0:
+                # Check that there are not errenous orders lingering
+                while (
+                    len(open_orders_heap) > 0
+                    and open_orders_heap[0].arrival_date < time_step
+                ):
+                    logger.error(
+                        f"A stale order was found in the system of {open_orders_heap[0]}.  Please look into what process placed this order."
+                    )
+                    heapq.heappop(open_orders_heap)
+
+                # Check if there are any orders which are arriving at the current time step.
+                while (
+                    len(open_orders_heap) > 0
+                    and open_orders_heap[0].arrival_date == time_step
+                ):
+                    received_order = heapq.heappop(open_orders_heap)
+                    logger.debug(f"Received the following order: {received_order}.")
+                    current_stock += received_order.units_ordered
+
+            # Perform the activity of sales for the given time period
+
+            # Record information about sales activity that occured
+            # This could include potential for losses.
+            if current_stock >= sales:
+                current_stock -= sales
+                sales_record = SalesRecord(date=time_step, units_sold=sales)
+                simulation_data["units_sold"].append(sales_record)
+
+            else:
+
+                sales_record = SalesRecord(date=time_step, units_sold=current_stock)
+                simulation_data["units_sold"].append(sales_record)
+
+                loss_record = LossRecord(
+                    date=time_step, units_lost=abs(sales - current_stock)
+                )
+                simulation_data["units_lost"].append(loss_record)
+                logger.debug(
+                    f"Failed to complete {loss_record.units_lost} sales on time_step {loss_record.date}."
+                )
+
+                current_stock = 0
+
+            # Add the end of time step status to the records.
+            end_of_time_step_record = EndOfTimeStepRecord(
+                date=time_step, ending_stock=current_stock
+            )
+            simulation_data["time_step_stock_counts"].append(end_of_time_step_record)
+
+            # Perform a check at the end of the day to see if an order should be placed.
+
+            if time_step % inventory_check_frequency == 0:
+                # NOTE: For now we will limit to only allowing 1 order to be placed at a time.
+                if len(open_orders_heap) > 0:
+                    continue
+                restock_order = self.threshold_restock_strategy(
+                    current_stock,
+                    current_time_step=time_step,
+                    **strategy_params,
+                )
+                if restock_order is not None:
+                    logger.debug(f"The following order has been placed {restock_order}.")
+                    simulation_data["orders_placed"].append(restock_order)
+                    heapq.heappush(open_orders_heap, restock_order)
+
+        return simulation_data
+
+  
